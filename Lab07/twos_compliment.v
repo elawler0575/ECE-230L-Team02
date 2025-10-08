b748b8b54c48acde204038f2842fa75d27daabf1
elawler0575 <emalinelawler@u.boisestate.edu>
2025-10-07 19:27:27 -0600

    Update twos_compliment.v

--- Lab07/twos_compliment.v
+++ Lab07/twos_compliment.v
@@ -2,17 +2,21 @@ module twos_compliment(
     input [7:0] A,
     output [7:0] B
     );
-
-ones_compliment onetotwo(
-    .A(A[7:0]),
-    .Y(B[7:0])
-    );
     
-full_adder addone(
-    .A(B[7:0]),
+full_adder fulladder(
+    .A(A[0]),
     .B(1'b1),
     .Cin(1'b0),
-    .Y(B[7:0])
+    .Y(B[0]),
+    .Cout(wire)
+
+    for (i = 1; i < 7; i = i + 1) begin
+        .A(A[i]),
+        .B(1'b0),
+        .Cin(Cout[i-1]),
+        .Y(B[i]),
+        .Cout(wire)
     );
+
 
 endmodule
