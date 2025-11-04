be5c36f41ccdac69a3ac0984dcaf79ffe296b64b
AnnikaCamarillo <156105262+AnnikaCamarillo@users.noreply.github.com>
2025-11-04 07:43:16 -0700

    Update eightbitdlatch.v

--- Lab10/eightbitdlatch.v
+++ Lab10/eightbitdlatch.v
@@ -1,5 +1,7 @@
 module eightbitdlatch(
     input E,D,
+    // input E, input [7:0]D 
+    // D was orginally one bit so chnaged to 8 bit array get rid of code on line 2
     output [7:0]Q, [7:0]notQ
     );
     
@@ -8,7 +10,7 @@ module eightbitdlatch(
         for (i = 0; i<8 ; i = i +1) begin
             dlatch dlatch(
             .E(E),
-            .D(D),
+            .D(D), //change to .D(D[i])
             .Q(Q[i]),
             .notQ(notQ[i])
          );
